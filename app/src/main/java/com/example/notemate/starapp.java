9ea6310d238e7577090a849f75a66bd8db42617f
Ritik Kumar Pandey <ritikpandey2610@gmail.com>
2023-02-24 22:05:47 +0530

    initial commit

--- app/src/main/java/com/example/notemate/starapp.java
+++ app/src/main/java/com/example/notemate/starapp.java
@@ -7,22 +7,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
-  AppCompatButton btn = findViewById(R.id.f1btn);
+public class starapp extends AppCompatActivity {
+
+    AppCompatButton btn=findViewById(R.id.f2btn);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_starapp);
 
         Intent inext;
-        inext=new Intent(MainActivity.this, starapp.class);
+        inext=new Intent(starapp.this, loginpage.class);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               startActivity(inext);
+                startActivity(inext);
             }
         });
-
     }
 }
